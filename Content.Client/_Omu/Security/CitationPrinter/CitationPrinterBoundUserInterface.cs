95276aac5c63133f97576a6cca36084c8d90b6d1
JWOSTeam <1steveminecraft1@gmail.com>
2026-09-21 16:40:47 -0500

    Oops, populated empty file

---
+++ Content.Client/_Omu/Security/CitationPrinter/CitationPrinterBoundUserInterface.cs
@@ -0,0 +1,69 @@
+using System;
+using Content.Shared._Omu.CitationPrinter;
+using Robust.Client.UserInterface;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Timing;
+
+namespace Content.Client._Omu.CitationPrinter;
+
+public sealed class CitationPrinterBoundUserInterface : BoundUserInterface
+{
+    private CitationPrinterWindow? _window;
+    private int _stateRevision;
+
+    public CitationPrinterBoundUserInterface(EntityUid owner, Enum uiKey)
+        : base(owner, uiKey)
+    {
+    }
+
+    protected override void Open()
+    {
+        base.Open();
+
+        _window = this.CreateWindow<CitationPrinterWindow>();
+
+        _window.PrintRequested += (name, offense) =>
+            SendMessage(new CitationPrinterPrintMessage(name, offense));
+
+        _window.RefreshRequested += () =>
+            SendMessage(new CitationPrinterRefreshMessage());
+    }
+
+    protected override void UpdateState(BoundUserInterfaceState state)
+    {
+        base.UpdateState(state);
+
+        if (state is not CitationPrinterUiState printerState
+            || _window is not { } window)
+        {
+            return;
+        }
+
+        var revision = ++_stateRevision;
+
+        window.SetIssuer(printerState.IssuerName);
+
+        if (printerState.RemainingCooldown <= TimeSpan.Zero)
+        {
+            window.SetPrintReady(true);
+            return;
+        }
+
+        window.SetPrintReady(false);
+
+        Timer.Spawn(printerState.RemainingCooldown, () =>
+        {
+            if (_stateRevision != revision || _window != window)
+                return;
+
+            window.SetPrintReady(true);
+        });
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        ++_stateRevision;
+        _window = null;
+        base.Dispose(disposing);
+    }
+}
